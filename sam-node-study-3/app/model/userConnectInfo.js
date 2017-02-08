b83f2b30fa86e7b5b698e953351680af0e9649dc
박새미 <sam@bagsaemiui-MacBook-Pro-2.local>
2017-02-08 14:36:21 +0900

    인증관련 모델정보 추가

---
+++ sam-node-study-3/app/model/userConnectInfo.js
@@ -0,0 +1,26 @@
+const sequelize = require('sequelize');
+const db = require('./mysql.connection');
+
+const UserConnectInfo = db.define('userConnectInfo', {
+        userId: {
+            type: sequelize.STRING(45),
+            primaryKey: true,
+            references: {
+                model: 'userInfo',
+                key: 'id'
+            }
+        },
+        accessToken: sequelize.CHAR(64),
+        expiredTime: sequelize.DATE
+    },
+    {
+        createdAt: false,
+        updatedAt: false,
+        tableName: 'userConnectInfo',
+        indexes: [
+            {fields: ['accessToken']}
+        ]
+    }
+);
+
+module.exports = UserConnectInfo;
--- sam-node-study-3/app/models.js
+++ sam-node-study-3/app/models.js
@@ -4,8 +4,20 @@ const db = require('./model/mysql.connection');
  * MODEL
  */
 const UserInfo = require('./model/userInfo');
+const UserConnectInfo = require('./model/userConnectInfo');
+
+UserInfo.hasOne(UserConnectInfo, {
+   foreignKey: 'deviceId',
+    constraints: false
+});
+
+UserConnectInfo.belongsTo(UserInfo, {
+    foreignKey: 'userId',
+    constraints: false
+});
 
 module.exports = {
     db: db,
-    UserInfo: UserInfo
+    UserInfo: UserInfo,
+    UserConnectInfo: UserConnectInfo
 };
